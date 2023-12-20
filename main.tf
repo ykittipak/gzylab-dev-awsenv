9fc12d1027c1204643b6e5759f190a351241177b
ykittipak <kittipak.y@gmail.com>
2023-12-20 17:09:57 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_subnet" "private_subnets" {
  }
 }
 
- resource "aws_instance" "Test EC2" {
+ resource "aws_instance" "Test_EC2" {
     ami           = "ami-0464f90f5928bccb8"
     instance_type = "t3.micro"
 
